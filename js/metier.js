1e210f35bddd714c037f189f6ad7ed735272f9c2
Angeline YANG <118853069+Xiaolatus@users.noreply.github.com>
2025-08-29 10:17:09 +0200

    Add files via upload

--- js/metier.js
+++ js/metier.js
@@ -11,7 +11,8 @@ const acData = [
       "lien": "J’ai su développer et personnaliser un site dynamique en réponse à un besoin réel. L’utilisation de PluXML, bien qu’atypique par rapport à d’autres CMS plus répandus, m’a permis d’approfondir ma compréhension des systèmes de gestion de contenu tout en consolidant mes compétences en HTML, CSS et PHP.",      
       "progression": "Au début du projet, j’ai rencontré des difficultés à comprendre la structure interne de PluXML et ses mécanismes de gestion des contenus. Grâce à la documentation et à une phase de tests, j’ai progressivement surmonté ces obstacles. Cette expérience m’a permis d’apprendre à chercher des solutions par moi-même, à m’adapter à des outils existants et à traduire des attentes métiers en fonctionnalités concrètes.",
       "images": [
-        { "src": "img/accueil.png", "alt": "maquettes" }
+        { "src": "img/accueil.png", "alt": "maquettes" },
+        { "src": "img/interface.png", "alt": "PluXML" },
       ],
   }
   },
@@ -25,7 +26,10 @@ const acData = [
       "contribution": "J’ai pris en charge le projet de bout en bout. J’ai d’abord analysé les besoins avec les enseignants référents afin de bien comprendre leurs attentes. J’ai ensuite choisi l’outil le plus adapté, PluXML, pour sa légèreté et sa simplicité de maintenance. J’ai planifié et priorisé les différentes étapes du projet, en commençant par la structure du site, puis en développant les fonctionnalités dynamiques avant d’assurer la mise en production. Tout au long du projet, j’ai communiqué régulièrement avec les parties prenantes afin de vulgariser mes choix techniques et de m’assurer que les solutions proposées correspondaient à leurs besoins. Enfin, j’ai rédigé une documentation claire et adaptée au personnel administratif, afin de garantir leur autonomie dans la gestion et la mise à jour du site après mon départ.",
       "lien": "Cette expérience illustre ma capacité à gérer un projet web complet, bien au-delà du simple développement technique. J’ai su traduire des besoins exprimés par des interlocuteurs non techniques en solutions concrètes, tout en assurant la pérennité et la qualité du site livré.",
       "progression": "J’ai su démontrer ma capacité à prendre en charge un projet web de A à Z, en conciliant contraintes techniques, attentes utilisateurs et respect des délais. Ce projet m’a aussi permis de développer mes compétences en communication avec des interlocuteurs non spécialistes, en expliquant des concepts techniques de manière claire. J’ai progressé dans la planification, la rédaction de documentation fonctionnelle, et la gestion de la maintenance.",
-  }
+      "images": [
+        { "src": "img/tuto.png", "alt": "tutoriel" }
+      ],  
+    }
   },
 ];
 
