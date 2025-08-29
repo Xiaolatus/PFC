30a3606b21937523e90081bdb7bba16375a4c81c
Angeline YANG <118853069+Xiaolatus@users.noreply.github.com>
2025-08-29 10:36:32 +0200

    Add files via upload

---
+++ js/entreprendre.js
@@ -0,0 +1,82 @@
+// AC Entreprendre
+const acData = [
+  {
+    "id": "ac35-01",
+    "titre": "AC35.01 — Piloter un produit, un service ou une équipe",
+    "projet": "R601 - Entreprenariat - Click'n Wash",
+    "logo": "img/click.png",
+    "contenu": {
+      "contexte": "J’ai travaillé sur le projet Click’n Wash, un service innovant de laverie communautaire. L’idée reposait sur la création d’une plateforme permettant la réservation en ligne, l’envoi de notifications par SMS et la mise en place d’un réseau d’entraide entre utilisateurs. Ce projet impliquait une approche globale : définition des fonctionnalités du service, élaboration d’un budget prévisionnel, modélisation de la monétisation et projection des bénéfices.",
+      "contribution": "J’ai activement contribué à la conception du projet en proposant des solutions techniques et organisationnelles. Mon rôle a notamment consisté à piloter la planification du projet et organiser le travail collaboratif, proposer des idées pour améliorer l’expérience utilisateur et la viabilité économique, et participer à la rédaction du rendu final, en veillant à la clarté et à la cohérence de la présentation.",
+      "lien": "Ce projet m’a permis de piloter un produit et une équipe, en adoptant une démarche structurée et en coordonnant efficacement les contributions de chaque membre. J’ai expérimenté la posture de chef de projet, garantissant la progression collective et la qualité du livrable final.",
+      "progression": "J’ai démontré une capacité à organiser et coordonner les actions de l’équipe dans un délai limité, tout en anticipant les besoins et en gérant les priorités. Ce travail m’a également confronté à la nécessité de résoudre rapidement certains problèmes organisationnels. Les axes de progression concernent l'amélioration de l’efficacité et la communication au sein de équipe.",
+      "metier": "Lors de mon stage avec PluXML, j’ai retrouvé ces problématiques de pilotage, mais dans un contexte plus technique : gérer l’évolution d’un site institutionnel, coordonner avec une équipe existante et adapter les livrables aux besoins de l’organisation. L’expérience acquise avec Click’n Wash m’a ainsi permis d’aborder le monde professionnel avec une meilleure vision globale de la gestion de projet, en comprenant comment relier la stratégie de service (côté entrepreneurial) et la réalisation technique.",
+    }
+  },
+  {
+    "id": "ac35-02",
+    "titre": "AC35.02 — Maîtriser la qualité en projet Web ou multimédia",
+    "projet": "SAE 501 - Développer pour le web ou Concevoir un dispositif interactif - Au fil de l'Ourcq",
+    "logo": "img/ourcq.png",
+    "contenu": {
+      "contexte": "Dans le cadre de la SAE 501, j’ai participé au développement d’une application touristique interactive sur le canal de l’Ourcq, destinée à l’organisme de tourisme de la Seine-Saint-Denis. Ce projet avait une dimension professionnelle forte car il répondait à des besoins et attentes réels d’un commanditaire externe, avec des contraintes précises en termes de design, d’ergonomie et de fonctionnalités.",
+      "contribution": "J'ai conçu la structure de l'application, développé la navigation entre les différents écrans et intégré plusieurs fonctionnalités : quiz interactifs, menu dynamique et système de QR codes. J'ai également participé à la rédaction de la documentation technique afin d'assurer la compréhension et la maintenabilité du projet.",
+      "lien": "Ce projet m’a permis de mettre en œuvre une démarche qualité dans un projet multimédia réel : respect des chartes graphiques et ergonomiques, prise en compte de l’expérience utilisateur, validation des parcours et optimisation de l’accessibilité sur mobile",
+      "progression": "J’ai su appliquer des normes UX/UI et respecter les contraintes fixées par le client. Le produit livré garantissait une interface cohérente, intuitive et adaptée aux usages mobiles. Ce projet m’a permis de progresser dans l’application de bonnes pratiques de conception centrées utilisateur. Mes axes de progression concernent la mise en place de tests utilisateurs plus systématiques et l’intégration d’outils de suivi qualité (par exemple, évaluation de la performance ou validation automatique de l’accessibilité).",
+      "metier": "Lors de mon stage avec PluXML, la maîtrise de la qualité passait également par le respect de standards visuels et fonctionnels imposés par une institution. L’expérience acquise dans le cadre de la SAE 501 m’a aidé à mieux appréhender ces enjeux professionnels : garantir la cohérence d’un produit numérique tout en intégrant les attentes du client. Le parallèle entre ces deux contextes met en évidence la complémentarité entre les apprentissages de l’IUT et leur application en entreprise.",
+      "images": [
+        { "src": "img/app.png", "alt": "maquettes" }
+      ],
+    }
+  },
+  {
+    "id": "ac35-03",
+    "titre": "AC35.03 — Concevoir un projet d’entreprise innovante en définissant le nom, l’identité, la forme juridique et le ton de la marque",
+    "projet": "R601 - Entreprenariat - Click'n Wash",
+    "logo": "img/click.png",
+    "contenu": {
+      "contexte": "Dans le cadre du module R601 « Entrepreneuriat », j’ai participé à un atelier de conception express autour d’un projet d’entreprise innovante : Click’n Wash, un service de laverie communautaire avec réservation en ligne, notifications SMS et mise en relation entre utilisateurs. L’objectif de l’atelier était de structurer un projet complet, allant de la définition du concept et de la cible à l’élaboration d’un budget prévisionnel et d’une projection des bénéfices. Nous avons également réfléchi à l’identité du service, à sa forme juridique potentielle et au ton de la marque.",
+      "contribution": "J’ai contribué à la conception du service en participant à la définition du nom et de l’identité de marque, mais aussi en travaillant sur la cohérence entre l’offre proposée, la cible visée et le modèle économique. J’ai pris part à l’analyse financière en réalisant une première estimation des coûts et bénéfices attendus, ce qui a permis de valider la faisabilité du projet.",
+      "lien": "Il ne s’agissait pas uniquement d’imaginer une idée de service, mais de la formaliser en tant que projet entrepreneurial viable. Cela impliquait de définir la proposition de valeur, de réfléchir à la structure juridique et de donner une identité claire à la marque, en cohérence avec le marché cible.",
+      "progression": "J’ai appris à équilibrer les aspects économiques et techniques d’un service numérique. L’expérience m’a permis de comprendre l’importance d’une analyse financière rigoureuse pour crédibiliser une idée innovante.Les axes d’amélioration portent sur l’approfondissement des aspects légaux et administratifs liés à la création d’entreprise (choix du statut, gestion fiscale, aspects réglementaires) et sur une étude de marché plus poussée pour affiner la stratégie de lancement.",
+    }
+  },
+  {
+    "id": "ac35-04",
+    "titre": "AC35.04 — Défendre un projet de manière convaincante",
+    "projet": "SAE 501 - Développer pour le web ou Concevoir un dispositif interactif - Au fil de l'Ourcq",
+    "logo": "img/ppt.png",
+    "contenu": {
+      "contexte": "Dans le cadre de la SAE 501, notre groupe a présenté devant un jury l’application touristique interactive développée pour l’organisme de tourisme de la Seine-Saint-Denis, intitulée Au fil de l’Ourcq. Ce projet avait pour objectif de valoriser le canal de l’Ourcq à travers une application mobile intégrant des fonctionnalités interactives (quiz, menu dynamique, scanner QR code, etc.). Lors de la soutenance, j’ai exposé la partie technique sur laquelle j’avais contribué, notamment le développement sous Flutter et les choix UX/UI associés.",
+      "contribution": "Ma contribution a porté sur la présentation et la défense des aspects techniques du projet. J’ai expliqué la logique de navigation entre les pages, l’intégration des composants interactifs et les raisons de nos choix en termes d’ergonomie.",
+      "lien": "Cette expérience illustre pleinement l’AC35.04 car il s’agissait non seulement de présenter un livrable, mais surtout de convaincre un jury en articulant discours technique et communication claire. J’ai su adapter mon vocabulaire et mon argumentation afin de rendre accessibles des choix parfois complexes, et démontrer la valeur ajoutée de notre application.",
+      "progression": "Je pense avoir été clair et structuré dans ma prise de parole, avec un discours compréhensible même pour des interlocuteurs non spécialistes. Cet exercice a consolidé ma capacité à vulgariser des notions techniques et à les relier à des choix concrets en termes d’expérience utilisateur. Les axes d’amélioration concernent la gestion du stress et le développement d’une plus grande aisance à l’oral. Des exercices de prise de parole supplémentaires et un travail sur la posture et la respiration pourraient renforcer mon impact lors de futures présentations.",
+      "metier": "Dans mon stage avec PluXML, la communication faisait également partie des enjeux : il s’agissait de rendre compte de mes avancées et de mes choix techniques auprès d’une équipe non technique. Cette transversalité entre l’IUT et l’entreprise illustre l’importance de la communication claire et convaincante, compétence indispensable pour défendre ses propositions. La présentation devant toute l'équipe illustre également à cette apprentissage critique, le but est de défendre nos réalisations.",
+    }
+  }
+];
+
+// Génération des cartes
+const cardsContainer = document.getElementById("cards");
+acData.forEach(ac => {
+  const link = document.createElement("a");
+  link.href = "#" + ac.id;
+  link.className = "card-link";
+  link.dataset.ac = ac.id;
+
+  link.innerHTML = `
+    <article class="card">
+      <header class="card-head">
+        <h2>${ac.titre}</h2>
+      </header>
+      <div class="card-body">
+        <div class="card-text">
+          <p class="muted">${ac.projet}</p>
+        </div>
+        ${ac.logo ? `<img class="logo" src="${ac.logo}" alt="${ac.projet}">` : ""}
+      </div>
+    </article>
+  `;
+
+  cardsContainer.appendChild(link);
+});
